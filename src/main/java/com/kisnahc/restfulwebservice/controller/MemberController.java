fbba95646ac798c82eefd911266a7ca431dfa259
kisnahc.github.io <dev.kisnahc@gmail.com>
2021-09-23 22:51:02 +0900

    회원 조회 API 리팩토링

--- src/main/java/com/kisnahc/restfulwebservice/controller/MemberController.java
+++ src/main/java/com/kisnahc/restfulwebservice/controller/MemberController.java
@@ -1,7 +1,10 @@
 package com.kisnahc.restfulwebservice.controller;
 
 import com.kisnahc.restfulwebservice.domain.Member;
+import com.kisnahc.restfulwebservice.dto.MemberDto;
 import com.kisnahc.restfulwebservice.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +12,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,20 +20,33 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+        private int count;
+    }
+
     @GetMapping("/members")
-    public List<Member> findAll() {
-        return memberService.findAll();
+    public Result memberList() {
+        List<Member> memberList = memberService.findAll();
+
+        //엔티티 -> DTO 변환
+        List<MemberDto> collect = memberList.stream()
+                .map(m -> new MemberDto(m.getUsername()))
+                .collect(Collectors.toList());
+
+        return new Result(collect, memberList.size());
     }
 
     @GetMapping("/members/{id}")
-    public Member findById(@PathVariable Long id) {
+    public Member findMember(@PathVariable Long id) {
         return memberService.findById(id);
     }
 
     @PostMapping("/members")
     public ResponseEntity<Member> createMember(@RequestBody Member member) {
         Long saveMemberId = memberService.save(member);
-
 
         //Status 201 Created
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
---
+++ src/main/java/com/kisnahc/restfulwebservice/dto/MemberDto.java
@@ -0,0 +1,12 @@
+package com.kisnahc.restfulwebservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MemberDto {
+
+    private String username;
+
+}
