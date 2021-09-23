a8463de467605dd6109b6eabc9ef31e0af3250c6
kisnahc.github.io <dev.kisnahc@gmail.com>
2021-09-23 14:50:35 +0900

    lombok test

---
+++ src/main/java/com/kisnahc/restfulwebservice/bean/HelloBean.java
@@ -0,0 +1,10 @@
+package com.kisnahc.restfulwebservice.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HelloBean {
+    private String message;
+}
