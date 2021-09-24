ee640af39a92ca41175cdc1a37b33a1e4ee9485a
kisnahc.github.io <dev.kisnahc@gmail.com>
2021-09-24 15:14:12 +0900

    Internationalized(국제화) 추가.

--- src/main/java/com/kisnahc/restfulwebservice/RestfulWebServiceApplication.java
+++ src/main/java/com/kisnahc/restfulwebservice/RestfulWebServiceApplication.java
@@ -2,12 +2,23 @@ package com.kisnahc.restfulwebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestfulWebServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebServiceApplication.class, args);
+    }
+
+    @Bean
+    public CookieLocaleResolver cookieLocaleResolver() {
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+        cookieLocaleResolver.setDefaultLocale(Locale.KOREA); //기본값
+        return cookieLocaleResolver;
+    }
 
 }
--- src/main/java/com/kisnahc/restfulwebservice/controller/HelloController.java
+++ src/main/java/com/kisnahc/restfulwebservice/controller/HelloController.java
@@ -1,12 +1,20 @@
 package com.kisnahc.restfulwebservice.controller;
 
 import com.kisnahc.restfulwebservice.bean.HelloBean;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
+
+    private final MessageSource messageSource;
 
     @GetMapping("/hello-world")
     public String hello() {
@@ -23,5 +31,11 @@ public class HelloController {
     @GetMapping("/hello-world/{id}")
     public HelloBean helloPathVariable(@PathVariable String id) {
         return new HelloBean("hello ! " + id);
+    }
+
+    @GetMapping("/hello-internationalized")
+    public String helloInternationalized(
+            @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("hello", null, locale);
     }
 }
